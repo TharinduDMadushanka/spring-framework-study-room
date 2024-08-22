a537e8001ccac73fdd2f8f04d2319ec6d8bebc1e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-22 18:27:48 +0530

    ApplicationContext replace for BeanFactory

---
+++ Telusko/application-context/src/main/java/lk/practice/Main.java
@@ -0,0 +1,17 @@
+package lk.practice;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+//        BeanFactory factory = new ClassPathXmlApplicationContext("spring.xml");
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+
+        Alien obj = (Alien) factory.getBean("alien");
+        obj.code();
+    }
+}
