8402342d8b2cb2bcd9b09f9a98c1ba3da7db69df
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-24 21:22:31 +0530

    prototype beans

---
+++ Telusko/prototype-bean/src/main/java/lk/practice/Alien.java
@@ -0,0 +1,14 @@
+package lk.practice;
+
+public class Alien {
+
+    int age;
+
+    public Alien(){
+        System.out.println("Alien Object Created..!");
+    }
+
+    public void code(){
+        System.out.println("I'm coding..!");
+    }
+}
---
+++ Telusko/prototype-bean/src/main/java/lk/practice/Main.java
@@ -0,0 +1,21 @@
+package lk.practice;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+
+        Alien obj1 = (Alien) factory.getBean("alien");
+        obj1.code();
+        obj1.age=22;
+        System.out.println(obj1.age);
+
+        Alien obj2 = (Alien) factory.getBean("alien");
+        obj2.code();
+        System.out.println(obj2.age);
+    }
+}
