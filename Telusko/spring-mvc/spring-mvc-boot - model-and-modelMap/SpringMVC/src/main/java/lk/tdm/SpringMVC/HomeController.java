d1d0f4a68db85ae0820cf7ee7f524a9439f8a1f7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-26 14:06:00 +0530

    spring-mvc-boot accept user input with Model and ModelMap

---
+++ Telusko/spring-mvc/spring-mvc-boot - model-and-modelMap/SpringMVC/src/main/java/lk/tdm/SpringMVC/HomeController.java
@@ -0,0 +1,27 @@
+package lk.tdm.SpringMVC;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+//        System.out.println("Home Page requested.!");
+        return "index";
+    }
+
+    @RequestMapping("add")
+    public String add(@RequestParam("num1") int i,@RequestParam("num2") int j, Model m ){
+
+        int num3 = i+j;
+        m.addAttribute("num3",num3);
+
+        return "result";
+    }
+
+}
