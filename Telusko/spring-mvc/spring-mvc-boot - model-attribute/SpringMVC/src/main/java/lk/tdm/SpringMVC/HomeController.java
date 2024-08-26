574027e6ffafddf8c86b114575230ebeb9c06854
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-08-26 14:19:55 +0530

    spring-mvc-boot Model Attribute

---
+++ Telusko/spring-mvc/spring-mvc-boot - model-attribute/SpringMVC/src/main/java/lk/tdm/SpringMVC/HomeController.java
@@ -0,0 +1,40 @@
+package lk.tdm.SpringMVC;
+
+import lk.tdm.SpringMVC.model.Alien;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/home")
+    public String home(){
+        return "index";
+    }
+
+    /**
+    @RequestMapping("addAlien")
+    public String addAlien(@RequestParam("id") int id,@RequestParam("name") String name, Model m ){
+
+        Alien a = new Alien();
+        a.setId(id);
+        a.setName(name);
+
+        m.addAttribute("alien",a);
+
+        return "result";
+    }
+    */
+
+    @RequestMapping("addAlien")
+    public String addAlien(Alien a, Model m ){
+
+        m.addAttribute("alien",a);
+
+        return "result";
+    }
+
+}
